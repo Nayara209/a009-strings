fa862108b0fb601edb959b6cc2cd7f592af07f5d
Nayara <nayaraazevwdo@gmail.com>
2022-07-26 18:19:06 -0300

    meu-exercicio-009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,13 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//Removendo espaços
+console.log(minhaString.trim())
+
+//Quantidade de caracteres
+const novaString = "Eu não sou superticioso, mas sou um pouco ________."
+const tamanho2 = minhaString.length
+const tamanho = novaString.length
+console.log(tamanho)
+console.log(tamanho2)
+const novaFrase = minhaString.replace(" ________.", " sticioso ")
+
+console.log(novaFrase)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+ const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS" , mas não deixe o gato sair `
+ const minhaFrase = frase.replace(" verde " , " rosa " ).replace("azul" , "laranja")
+
+ console.log(minhaFrase)
+
+ const temVerdeLaranja = minhaFrase.includes("verde" , "laranja")
+ console.log("A nova string tem verde e laranja?" , temVerdeLaranja)
+ 
+ console.log(frase.replace("mas não deixe o gato sair", " MAS NÃO DEIXE O GATO SAIR"))
